0ecd0b3632b624e6a08a004cd488e79267fb9790
Felipe Brito <89111098+fefezoka@users.noreply.github.com>
2023-05-29 01:26:41 -0300

    please god

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.VERCEL_URL || 'http://localhost:3001',
+  baseURL: process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3001',
 });
 
 export default api;
