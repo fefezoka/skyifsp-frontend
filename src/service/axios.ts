921865db13b41e8fcb0002bfbccb3346ac6e2bb4
Felipe Brito <89111098+fefezoka@users.noreply.github.com>
2023-05-29 01:52:42 -0300

    Finally

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://skyifsp-backend.vercel.app/`
     : 'http://localhost:3001',
 });
 
