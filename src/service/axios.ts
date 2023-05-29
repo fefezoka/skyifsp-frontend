912476f61f2536dc47a8cbab7cad7c67545eb8bf
Felipe Brito <89111098+fefezoka@users.noreply.github.com>
2023-05-29 01:47:39 -0300

    el pichula is real

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : 'http://localhost:3001',
 });
 
